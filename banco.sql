c92adb146f036d8953fc4bf2e6e7cdffb7ceef29
Emanuel-Bissacotti <emanuelfbissacotti@gmail.com>
2023-11-30 09:18:39 -0300

    Corrige banco de dados
    Na teoria banco esta seguindo padrões do modelo.

--- banco.sql
+++ banco.sql
@@ -3,27 +3,27 @@ create database projetoASMAR;
 use projetoASMAR;
 
 create table Usuario(
-    id int auto_increment primary key,
-    nome varchar(30),
-    email varchar(30),
-    cargo varchar(30)
+	id bigint auto_increment primary key,
+	nome varchar(50),
+	email varchar(50),
+	cargo varchar(30)
 );
 
 create table Produtos(
-    id int auto_increment primary key,
-    drescricao varchar(30),
-    precoDeCusto int,
-    estoque int,
-    validade varchar(30)
+	id bigint auto_increment primary key,
+	descricao varchar(30),
+	precoDeCusto double,
+	estoque int,
+	validade date
 );
 
 create table EntradaSaidaProdutos(
-    id int auto_increment primary key,
-    idProduto int,
-    usuarioResponsavel int,
-    motivoEntradaOuSaida varchar(20)
-    quantidade int,
-    dataEHora varchar(30),
-    foreign key (idProduto) Produtos(id),
-    foreign key (usuarioResponsavel) Usuario(id)
+	id bigint auto_increment primary key,
+	idProduto bigint,
+	idUsuario bigint,
+	motivo varchar(20),
+	quantidade int,
+	dataEHora date,
+	foreign key (idProduto) references Produtos(id),
+	foreign key (idUsuario) references Usuario(id)
 );
